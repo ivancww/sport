7105dc5c13ace83311c8a1456a1e7b3762d236d7
ivancww <wahchiu0903@gmail.com>
2026-04-04 00:12:22 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'workout-v6-harbor';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'icon-512.png',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css',
+  'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500&display=swap'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
